b12dc9f91d0bf8d179247d9330f5b39ecb8363a6
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-01-25 13:40:54 +0530

    this method how to work up & down button it have be defined

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
+
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
